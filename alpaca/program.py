3dbdd950fc0d64c901c7ebc3e41171205f4d8f27
johnknelson <johnknelson3@yahoo.com>
2021-01-15 14:58:44 -0600

    add alpaca scripts and abstract away private credentials

--- alpaca/program.py
+++ alpaca/program.py
@@ -1,10 +1,13 @@
 import time
-from alpaca import evaluate_and_execute_trade
+from alpaca.trade import Trade
 
 trade_period = 60;
+trade = Trade()
 
+# TODO While market is open
 while True:
-    evaluate_and_execute_trade.evaluate_and_execute_trade()
+    print("Evaluating trade execution at: " + time.strftime('%a, %d %b %Y %H:%M:%S %Z(%z)'))
+    trade.evaluate_and_execute_trade()
     time.sleep(trade_period)
 
-
+# TODO After market closes, liquidate positions and exit script
--- alpaca/trade.py
+++ alpaca/trade.py
@@ -13,78 +13,83 @@ import smtplib
 
 # Ensenada
 from psar_ops import detect_sell_sar, detect_buy_sar
+import alpaca.constants as constants
 
-# TODO Pass in initialized API object so this init logic isn't executed every period
-def evaluate_and_execute_trade(self):
-    # Initialize Alpaca API
-    os.environ["APCA_API_BASE_URL"] = "https://paper-api.alpaca.markets"
-    # Insert API Credentials
-    api = tradeapi.REST('PKUZ9PNMHM4LEIYNXLH7', 'CxhOIUa8Hm1KSk3E7ZaXCqduErHZnsGNJQvR6GoA', api_version='v2')
-    account = api.get_account()
+class Trade:
 
-    # The mail addresses and password
-    sender_address = 'jkn3tradingalgo@gmail.com'
-    sender_pass = 'firepear'
-    receiver_address = 'johnknelson3@yahoo.com'
-    # Setup the MIME
-    message = MIMEMultipart()
-    message['From'] = 'Trading Bot'
-    message['To'] = receiver_address
-    message['Subject'] = 'Pairs Trading Algo'  # The subject line
+    # TODO Pass in initialized API object so this init logic isn't executed every period
+    def evaluate_and_execute_trade(self):
+        # Initialize Alpaca API
+        os.environ["APCA_API_BASE_URL"] = "https://paper-api.alpaca.markets"
+        # Insert API Credentials
+        api = tradeapi.REST(constants.ALPACA_KEY, constants.ALPACA_SECRET_KEY, api_version='v2')
+        account = api.get_account()
 
-    # Selection of stocks
-    minutes = 1000
-    stock = 'TSLA'
-    barset = api.get_barset(stock, 'minute', limit=minutes)
-    stock_bars = barset[stock]
+        # The mail addresses and password
+        sender_address = constants.SENDER_EMAIL_ADDRESS
+        sender_pass = constants.SENDER_EMAIL_PASSWORD
+        receiver_address = constants.RECEIVING_EMAIL_ADDRESS
+        # Setup the MIME
+        message = MIMEMultipart()
+        message['From'] = 'Ensenada'
+        message['To'] = receiver_address
+        message['Subject'] = 'Order activity'  # The subject line
 
-    # Calculate PSAR
-    acc_step = .02
-    acc_max = .2
-    price_open = stock_bars.df['open'].to_numpy()
-    price_close = stock_bars.df['close'].to_numpy()
-    price_high = stock_bars.df['high'].to_numpy()
-    price_low = stock_bars.df['low'].to_numpy()
-    psar = ti.psar(high=price_high, low=price_low, acceleration_factor_step=acc_step,
-                   acceleration_factor_maximum=acc_max)
+        # Selection of stocks
+        minutes = 1000
+        stock = 'TSLA'
+        barset = api.get_barset(stock, 'minute', limit=minutes)
+        stock_bars = barset[stock]
 
-    # Trading_algo
-    portfolio = api.list_positions()
-    holding = any(x.symbol == stock for x in portfolio)
-    clock = api.get_clock()
+        # Calculate PSAR
+        acc_step = .02
+        acc_max = .2
+        price_open = stock_bars.df['open'].to_numpy()
+        price_close = stock_bars.df['close'].to_numpy()
+        price_high = stock_bars.df['high'].to_numpy()
+        price_low = stock_bars.df['low'].to_numpy()
+        psar = ti.psar(high=price_high, low=price_low, acceleration_factor_step=acc_step,
+                       acceleration_factor_maximum=acc_max)
 
-    # If markets are open
-    mail_content = ""
-    if clock.is_open == True:
-        #TODO If not holding
-        if holding == False:
-            #TODO Detect should buy
-            if detect_buy_sar.detect_buy_sar(psar[len(psar) - 1], price_close[price_close.size-1]):
-                # buy 1 share
-                api.submit_order(symbol=stock, qty=1, side='buy', type='market', time_in_force='day')
-                mail_content = stock + " has been bought."
-        #TODO Else holding
+        # Trading_algo
+        portfolio = api.list_positions()
+        holding = any(x.symbol == stock for x in portfolio)
+        clock = api.get_clock()
+
+        # If markets are open
+        mail_content = ""
+        if clock.is_open == True:
+            #If not holding
+            if holding == False:
+                # Detect should buy
+                if detect_buy_sar.detect_buy_sar(psar[len(psar) - 1], price_close[price_close.size-1]):
+                    # buy 1 share
+                    api.submit_order(symbol=stock, qty=1, side='buy', type='market', time_in_force='day')
+                    buy_action_message = "Ordered buy of " + stock + " at " + str(price_close[price_close.size-1])
+                    print(buy_action_message)
+                    mail_content = buy_action_message
+            # Else holding
+            else:
+                # Detect should sell
+                if detect_sell_sar.detect_sell_sar(psar[len(psar) - 1], price_close[price_close.size-1]):
+                    # sell 1 share
+                    api.submit_order(symbol=stock, qty=1, side='sell', type='market', time_in_force='day')
+                    sell_action_message = "Sell has been ordered for" + stock + " with most recent close price " + str(price_close[price_close.size-1])
+                    print(sell_action_message)
+                    mail_content = sell_action_message
         else:
-            # TODO Detect should sell
-            if detect_sell_sar.detect_sell_sar(psar[len(psar) - 1], price_close[price_close.size-1]):
-                # sell 1 share
-                api.submit_order(symbol=stock, qty=1, side='sell', type='market', time_in_force='day')
-                mail_content = stock + " has been sold."
-    else:
-        mail_content = "The Market is Closed"
+            print("The Market is Closed")
+            mail_content = "The Market is Closed"
 
-    # Only send mail if an action was taken
-    if mail_content:
-        # The body and the attachments for the mail
-        message.attach(MIMEText(mail_content))
-        # Create SMTP session for sending the mail
-        session = smtplib.SMTP('smtp.gmail.com', 587)  # use gmail with port
-        session.starttls()  # enable security
-        session.login(sender_address, sender_pass)  # login with mail_id and password
-        text = message.as_string()
-        session.sendmail(sender_address, receiver_address, text)
-        session.quit()
-
-trade = evaluate_and_execute_trade()
-trade.evaluate_and_execute_trade()
+        # Only send mail if an action was taken
+        if mail_content:
+            # The body and the attachments for the mail
+            message.attach(MIMEText(mail_content))
+            # Create SMTP session for sending the mail
+            session = smtplib.SMTP('smtp.gmail.com', 587)  # use gmail with port
+            session.starttls()  # enable security
+            session.login(sender_address, sender_pass)  # login with mail_id and password
+            text = message.as_string()
+            session.sendmail(sender_address, receiver_address, text)
+            session.quit()
 
